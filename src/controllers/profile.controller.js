1834f10bf4f0c46948737575c4e1dd3b74e9b367
mrcoder57 <tiwariji2300@gmail.com>
2024-03-21 20:55:33 +0530

    change response to object

--- src/controllers/profile.controller.js
+++ src/controllers/profile.controller.js
@@ -1,23 +1,37 @@
 import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
+
 const createUserProfile = async (req, res) => {
   const userId = req.userId;
   try {
-    const { bio, linkedin, twitter, profilePic, skills, education, name, overview } = req.body;
+    const {
+      bio,
+      linkedin,
+      twitter,
+      profilePic,
+      skills,
+      education,
+      name,
+      overview,
+    } = req.body;
 
     if (!bio && !linkedin) {
-      return res.status(400).json({ error: "Please provide bio or linkedin or both" });
+      return res
+        .status(400)
+        .json({ error: "Please provide bio or linkedin or both" });
     }
 
     const existingProfile = await prisma.profile.findUnique({
       where: {
-        userId: userId 
-      }
+        userId: userId,
+      },
     });
 
     if (existingProfile) {
-      return res.status(400).json({ error: "Profile already exists for this user" });
+      return res
+        .status(400)
+        .json({ error: "Profile already exists for this user" });
     }
 
     const newProfile = await prisma.profile.create({
@@ -26,10 +40,10 @@ const createUserProfile = async (req, res) => {
         linkedin,
         twitter,
         profilePic,
-        skills, 
-        education, 
-        name, 
-        overview, 
+        skills,
+        education,
+        name,
+        overview,
         userId,
       },
     });
@@ -37,35 +51,25 @@ const createUserProfile = async (req, res) => {
     return res.status(201).json(newProfile);
   } catch (error) {
     console.error(error);
-    return res.status(500).json({ error: "Internal Server Error", message: error.message });
+    return res
+      .status(500)
+      .json({ error: "Internal Server Error", message: error.message });
   }
 };
-
-
 
 const getProfileByUserId = async (req, res) => {
-  const userId = parseInt(req.params.userId, 10); 
+  const userId = parseInt(req.params.userId, 10);
   try {
-      const profiles = await prisma.profile.findMany({
-          where: { userId },
+    const profiles = await prisma.profile.findFirst({
+      where: { userId },
+    });
 
-          
-      });
-
-      if (!profiles || profiles.length === 0) {
-          return res.status(404).json({ error: "Profiles not found for this user" });
-      }
-
-      return res.status(200).json({ profiles });
+    return res.status(200).json({ profiles });
   } catch (error) {
-      return res.status(500).json({ error: "An error occurred while fetching the profiles", error });
+    return res
+      .status(500)
+      .json({ error: "An error occurred while fetching the profiles", error });
   }
 };
 
-  
-
-  export {
-    createUserProfile,
-    getProfileByUserId
-  }
-  
+export { createUserProfile, getProfileByUserId };
